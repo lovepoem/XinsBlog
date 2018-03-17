a4de05111b29feb414e2d2d70caf6d3b3e917940
WangXin <lovepoem@hotmail.com>
2018-03-18 02:37:41 +0800

    修改2fa后的问题

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+npm uninstall hexo
+npm install hexo --no-optional
+npm uninstall hexo-cli -g
+npm install hexo-cli -g
 npm install hexo-deployer-git --save
+npm install hexo-toc --save
 hexo generate
 hexo deploy
