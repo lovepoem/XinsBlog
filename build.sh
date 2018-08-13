b290e7d489406e2d00317628e3cce2dfee3ce3f4
WangXin <xin.victorwang@gmail.com>
2018-08-13 13:18:44 +0800

    添加sitemap

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ npm uninstall hexo-cli -g
 npm install hexo-cli -g
 npm install hexo-deployer-git --save
 npm install hexo-toc --save
+npm install hexo-generator-sitemap --save
+npm install hexo-generator-baidu-sitemap --save
 hexo generate
 hexo deploy
