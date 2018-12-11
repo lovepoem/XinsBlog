49356e59e102580e537d0e168671ac76792c31ce
Xin Wang <xin.victorwang@gmail.com>
2018-12-11 17:25:28 +0800

    添加评论

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ npm install hexo-deployer-git --save
 npm install hexo-toc --save
 npm install hexo-generator-sitemap --save
 npm install hexo-generator-baidu-sitemap --save
-npm install --save hexo-autoprefixer hexo-filter-cleanup hexo-generator-feed hexo-renderer-sass hexo-renderer-swig mamboer/lunr.js moment node-sass object-assign
+npm install --save hexo-autoprefixer gitalk hexo-filter-cleanup hexo-generator-feed hexo-renderer-sass hexo-renderer-swig mamboer/lunr.js moment node-sass object-assign
 hexo generate
 hexo deploy
