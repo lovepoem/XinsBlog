4d4c04712ad3fc9ee48493a3ec5e730c8afc43b1
wangxin03 <wangxin03@weidian.com>
2018-12-24 19:58:29 +0800

    添加删除原来的缓存

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ npm install hexo-toc --save
 npm install hexo-generator-sitemap --save
 npm install hexo-generator-baidu-sitemap --save
 npm install --save hexo-autoprefixer gitalk hexo-filter-cleanup hexo-generator-feed hexo-renderer-sass hexo-renderer-swig mamboer/lunr.js moment node-sass object-assign
+rm -rf public/*
 hexo generate
 hexo deploy
