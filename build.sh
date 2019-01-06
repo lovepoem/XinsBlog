51feb90a011ffac4ba4c558f783dab9e39aa7158
Xin Wang <xin.victorwang@gmail.com>
2019-01-06 22:04:56 +0800

    修改题目

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ npm install hexo-generator-sitemap --save
 npm install hexo-generator-baidu-sitemap --save
 npm install --save hexo-autoprefixer gitalk hexo-filter-cleanup hexo-generator-feed hexo-renderer-sass hexo-renderer-swig mamboer/lunr.js moment node-sass object-assign
 #rm -rf public/*
-hexo generate
+hexo generate   
 hexo deploy
