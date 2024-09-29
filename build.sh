2a03681bddd898d4d08445ec262010dfdab286bc
Xin Wang <xin.victorwang@gmail.com>
2024-09-29 16:55:20 +0800

    更新修改

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 npm uninstall hexo
-npm install hexo --no-optional
+npm install hexo@4.0.0 --no-optional
 npm uninstall hexo-cli -g
-npm install hexo-cli -g
+npm install hexo-cli@2.0.0 -g
 npm install hexo-deployer-git --save
 npm install hexo-toc --save
 npm install hexo-generator-sitemap --save
 npm install hexo-generator-baidu-sitemap --save
 npm install hexo-generator-baidu-urls --save
-npm install --save hexo-autoprefixer gitalk hexo-filter-cleanup hexo-generator-feed hexo-renderer-sass hexo-renderer-swig mamboer/lunr.js moment node-sass object-assign
+npm install --save hexo-autoprefixer gitalk hexo-filter-cleanup hexo-generator-feed hexo-renderer-sass hexo-renderer-swig moment node-sass object-assign
 rm -rf public/*
 hexo generate
-# hexo deploy
+hexo deploy
