a275d25a88a650cf4e5fc7f24b9f78c6df5c0eaf
Xin Wang <xin.victorwang@gmail.com>
2024-10-10 12:58:24 +0800

    更新依赖版本

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ npm install hexo-toc --save
 npm install hexo-generator-sitemap --save
 npm install hexo-generator-baidu-sitemap --save
 npm install hexo-generator-baidu-urls --save
-npm install --save hexo-autoprefixer gitalk hexo-filter-cleanup hexo-generator-feed hexo-renderer-sass hexo-renderer-swig moment node-sass object-assign
+npm install --save hexo-autoprefixer gitalk hexo-filter-cleanup hexo-generator-feed hexo-renderer-sass hexo-renderer-swig lunr moment node-sass object-assign
 rm -rf public/*
 hexo generate
 hexo deploy
